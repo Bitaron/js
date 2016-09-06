f6e70892b38700f172429bb59233559d2f391a36
bitaron <bitaron90@gmail.com>
2016-09-06 16:57:49 +0600

    Node version checker added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # weatherApp
+Before Setup :
+1) Install node
+2) Install git.
+3) Clone from : 
--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -1,10 +1,22 @@
 (function(){
     "use strict";
 
+    var version = require("../util/version_checker.js");
+
     var startTime = Date.now();
-    task("default",function(){
+    task("default",["version"],function(){
         var elapsedSeconds = (Date.now() - startTime)/1000;
         console.log("\n\n Build ok ("+ elapsedSeconds.toFixed(2) + "s)");
     });
+
+    task("version",function(){
+       console.log("Check node version");
+        version.check({
+            name: "Node",
+            expected: require("../../package.json").engines.node,
+            actual: process.version,
+            strict: strict
+        }, complete, fail);
+    }, { async: true });
 
 }());
