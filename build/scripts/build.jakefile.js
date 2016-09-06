c18e08928a8ef189a04ab050e07a5e524b66b75b
bitaron <bitaron90@gmail.com>
2016-09-06 19:33:14 +0600

    Add place holder for linting

--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -3,19 +3,45 @@
 
     var version = require("../util/version_checker.js");
 
+    desc("Lint and Test");
     var startTime = Date.now();
-    task("default",["version"],function(){
+    task("default",["version","lint"],function(){
         var elapsedSeconds = (Date.now() - startTime)/1000;
         console.log("\n\n Build ok ("+ elapsedSeconds.toFixed(2) + "s)");
     });
 
+
+
+
+    //**LINT
+    
+    desc("Lint everyThing");
+    task("lint",["lintNode","lintClient"]);
+
+    desc("Lint Node");
+    task("lintNode",function(){
+        console.log("Linting node ..");
+    });
+
+    desc("Lint Client");
+    task("lintClient",function(){
+        console.log("Linting client ..");
+    });
+
+
+
+
+
+    //** CHECK VERSION
+
+    desc("Check node version");
     task("version",function(){
        console.log("Check node version");
         version.check({
             name: "Node",
             expected: require("../../package.json").engines.node,
             actual: process.version,
-            strict: strict
+            strict: false
         }, complete, fail);
     }, { async: true });
 
