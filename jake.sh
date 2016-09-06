7bd10a90f300ed9ff6b65dd5f6696144b368ec93
bitaron <bitaron90@gmail.com>
2016-09-06 11:54:34 +0600

    Jake build file added

--- jake.sh
+++ jake.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-jakeFile="/node_modules/.bin/jake"
-[ ! -f "$jakeFile" ] && echo "Building npm modules: ." && npm rebuild
-./node_modules/.bin/jake $*
+jakeFile="node_modules/.bin/jake"
+[ ! -f "$jakeFile" ] && echo "Building npm modules .." && npm rebuild
+./node_modules/.bin/jake -f build/scripts/build.jakefile.js $*
