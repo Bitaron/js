96d579bbd171c69eb4b7ef4efa416e78122b309d
bitaron <bitaron90@gmail.com>
2016-09-07 13:38:10 +0600

    Add sample react code

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 "use strict";
-function test(){
-    return 2;
-}
+// main.js
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('example')
+);
