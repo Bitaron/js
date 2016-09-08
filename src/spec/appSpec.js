e63d641187306f37efbf10e5cdeb8b7561ada791
bitaron <bitaron90@gmail.com>
2016-09-08 11:25:42 +0600

    Clean

--- src/spec/appSpec.js
+++ src/spec/appSpec.js
@@ -1,15 +1,2 @@
 
-var app = require("../app.js");
-var constant = require("../constants.js");
-
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
-    });
-
-    it("works on client code", function() {
-        expect(app.test()).toBe(2);
-    });
-});
-
 
