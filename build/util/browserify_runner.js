172b23308031e86b297a47cf692224fb93838897
bitaron <bitaron90@gmail.com>
2016-09-08 16:08:19 +0600

    Add 'react' support for 'browserify'  with 'reactify'.

--- build/util/browserify_runner.js
+++ build/util/browserify_runner.js
@@ -4,11 +4,13 @@
 var fs = require("fs");
 var path = require("path");
 var browserify = require("browserify");
+var reactify = require("reactify");
 
 
 
 exports.bundle = function(config, success, failure) {
     browserify(config.options)
+        .transform(reactify, {"es6": true})
         .require(config.entry, { entry: true })
         .bundle()
         .on("error",function(err){return failure(err);})
