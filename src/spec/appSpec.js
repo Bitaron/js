68bb5242ac4ca4abe70cb8a8d30d6c0daf245142
bitaron <bitaron90@gmail.com>
2016-09-08 16:24:25 +0600

    Insall react-addons-test-utils

--- src/spec/appSpec.js
+++ src/spec/appSpec.js
@@ -1,2 +1,19 @@
+'use strict';
 
+var TestUtils = require('react/addons').addons.TestUtils;
 
+describe("It is a spec ",function(){
+    it("to test react",function(){
+        var component = TestUtils.renderIntoDocument(
+            <TopLevelWrapper/>
+        );
+
+        var h1 = TestUtils.findRenderedDOMComponentWithTag(
+            component, 'h1'
+        );
+
+        console.log(h1.getDOMN)
+        expect(h1.getDOMNode().textContent)
+            .toEqual('Hello, world! 2');
+    })
+});
