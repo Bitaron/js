282fefac4e216aa87d3a789ebf4fe31e15aa0bb4
bitaron <bitaron90@gmail.com>
2016-09-08 10:55:53 +0600

    Refactor

--- build/util/jasmine_runner.js
+++ build/util/jasmine_runner.js
@@ -6,26 +6,25 @@ var JasmineConsoleReporter = require('jasmine-console-reporter');
 
 
 exports.run = function(config, success, failure) {
+
     var jasmine = new Jasmine();
-
     jasmine.loadConfig(config.jasmineVariables);
-
-    jasmine.onComplete(function(passed) {
-        if(passed) {
-            console.log('All specs have passed\n');
-        }
-        else {
-            console.log('At least one spec has failed');
-        }
-    });
-
-
 
     var reporter = new JasmineConsoleReporter(config.jasmineReporterVariable);
     jasmine.addReporter(reporter);
 
-
     jasmine.execute();
-    success();
+
+    jasmine.onComplete(function(passed) {
+        if(passed) {
+            console.log('All specs have passed\n');
+            success();
+        }
+        else {
+            console.log('At least one spec has failed');
+            failure();
+        }
+    });
+
 };
 
