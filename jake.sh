7146731b01bb0d43f4faf58338a9a03102d70392
bitaron <bitaron90@gmail.com>
2016-10-16 21:05:23 +0600

    Add continuous running test in build script

--- jake.sh
+++ jake.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
-jakeFile="node_modules/.bin/jake"
-[ ! -f "$jakeFile" ] && echo "Building npm modules .." && npm rebuild
-./node_modules/.bin/jake -f build/scripts/build.jakefile.js $*
+ #!/bin/bash
+ jakeFile="node_modules/.bin/jake"
+ [ ! -f "$jakeFile" ] && echo "Building npm modules .." && npm rebuild
+
+ if [ "$1" = "ctr" ]; then
+ echo "Running continuous test"
+ watch -c  -n1  ./node_modules/.bin/jake -f build/scripts/build.jakefile.js simpleTest --color
+ else
+ echo "Running jake"
+ ./node_modules/.bin/jake -f build/scripts/build.jakefile.js $*
+ fi
--- src/spec/floatSpec.js
+++ src/spec/floatSpec.js
@@ -7,7 +7,7 @@ var ExpectedData = require('./data/expected.data');
 describe("Create float using ", function() {
     it("separate significand and exponent", function() {
         var float = new Float(RealData.significand,RealData.exponent);
-        expect(float.equals(ExpectedData.FLOAT_DATA)).toBe(false);
+        expect(float.equals(ExpectedData.FLOAT_DATA)).toBe(true);
     });
 });
 
