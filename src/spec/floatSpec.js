4d3f0007a3103178a510227c518cdda7bbf71eb5
bitaron <bitaron90@gmail.com>
2016-10-19 11:06:31 +0600

    Refactor

--- src/spec/floatSpec.js
+++ src/spec/floatSpec.js
@@ -5,6 +5,7 @@ var TestData = require('../spec/data/test.data');
 var ExpectedData = require('./data/expected.data');
 
 describe('FLOAT PACKAGE TESTS ',function() {
+
     describe("Create float using ", function() {
         it("separated significand and exponent", function() {
             var float = Float.parse(TestData.CONSTRUCTOR_DATA.significand,
@@ -12,25 +13,35 @@ describe('FLOAT PACKAGE TESTS ',function() {
             expect(float.equals(ExpectedData.CONSTRUCTOR_DATA)).toBe(true);
         });
 
+
         it("using decimal number", function() {
+
             var first = Float.parseNumber(ExpectedData.OPERATION_DATA.CONVERSITION_DATA.FIRST);
-            // var firstMinus = Float.parseNumber(ExpectedData.OPERATION_DATA.CONVERSITION_DATA.FIRST_M);
-            var second = Float.parseNumber(ExpectedData.OPERATION_DATA.CONVERSITION_DATA.SECOND);
             expect(first.equals(TestData.OPERATION_DATA.CONVERTION_DATA.FIRST)).toBe(true);
+
+            // var firstMinus = Float.parseNumber(ExpectedData.OPERATION_DATA.CONVERSITION_DATA.FIRST_M);
             //expect(firstMinus.equals(TestData.OPERATION_DATA.CONVERTION_DATA.FIRST_M)).toBe(true);
+
+            var second = Float.parseNumber(ExpectedData.OPERATION_DATA.CONVERSITION_DATA.SECOND);
             expect(second.equals(TestData.OPERATION_DATA.CONVERTION_DATA.SECOND)).toBe(true);
+
+
         });
     });
+
 
     describe("Float can ",function() {
         it("coverts the map into single number", function() {
             var first = Float.convertToSingleNumber(TestData.OPERATION_DATA.CONVERTION_DATA.FIRST);
-            //var firstMinus = Float.convertToSingleNumber(TestData.OPERATION_DATA.CONVERTION_DATA.FIRST_M);
-            var second = Float.convertToSingleNumber(TestData.OPERATION_DATA.CONVERTION_DATA.SECOND);
             expect(first).toBe(ExpectedData.OPERATION_DATA.CONVERSITION_DATA.FIRST);
+
+            //var firstMinus = Float.convertToSingleNumber(TestData.OPERATION_DATA.CONVERTION_DATA.FIRST_M);
             // expect(firstMinus).toBe(ExpectedData.OPERATION_DATA.CONVERSITION_DATA.FIRST_M);
+
+            var second = Float.convertToSingleNumber(TestData.OPERATION_DATA.CONVERTION_DATA.SECOND);
             expect(second).toBe(ExpectedData.OPERATION_DATA.CONVERSITION_DATA.SECOND);
         });
+
 
         it("compare two numbers", function() {
             var greater = Float.compare(TestData.halfSpeedOfLightInFloat,
