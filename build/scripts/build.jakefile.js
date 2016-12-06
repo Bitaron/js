eca985842d8c0924227264ef9aa4959e5f355407
bitaron <bitaron90@gmail.com>
2016-12-06 11:49:09 +0600

    Add jake

---
+++ build/scripts/build.jakefile.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    desc('This is the default task.');
+    task('default', function () {
+        console.log('This is the default task.');
+    });
+    
+}());
---
+++ jake.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./node_modules/.bin/jake -f "build/scripts/build.jakefile.js" $*
