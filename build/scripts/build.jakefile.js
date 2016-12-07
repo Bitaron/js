0dee5fc970f799e6d2aa72f38fe6994f969eef97
bitaron <bitaron90@gmail.com>
2016-12-07 12:25:00 +0600

    Install fs and path for mocha

--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -12,7 +12,7 @@
         console.log("\n\nBuild ok (" + elapsedSeconds.toFixed(2) + "s)");
     });
 
-        //** NODE VERSION CHECK **//
+    //** NODE VERSION CHECK **//
     desc('checks node.js version');
     task('version', function () {
         console.log('Checking nodejs version ..');
@@ -24,12 +24,12 @@
         }, complete, fail);
     }, {async: true});
 
-                
-        //** LINT **//
+
+    //** LINT **//
     desc('lint complete source');
     task('lint', ['lintBuild', 'lintClient']);
 
-    
+
     desc('lint build source');
     task('lintBuild', function () {
         console.log('Linting build ..');
@@ -46,13 +46,39 @@
         }, complete, fail);
     });
 
-            //** TEST RUNNER **//
+    //** TEST RUNNER **//
     desc('run tests');
     task('tests', ['unitTests']);
-    
+
     desc('run unit test suits')
-    task('unitTests', function() {
+    task('unitTests', function () {
         console.log('Running unit tests ..');
+        var Mocha = require('mocha'),
+                fs = require('fs'),
+                path = require('path');
+
+// Instantiate a Mocha instance.
+        var mocha = new Mocha();
+
+        var testDir = 'some/dir/test'
+
+// Add each .js file to the mocha instance
+        fs.readdirSync(testDir).filter(function (file) {
+            // Only keep the .js files
+            return file.substr(-3) === '.js';
+
+        }).forEach(function (file) {
+            mocha.addFile(
+                    path.join(testDir, file)
+                    );
+        });
+
+// Run the tests.
+        mocha.run(function (failures) {
+            process.on('exit', function () {
+                process.exit(failures);  // exit with non-zero status if there were failures
+            });
+        });
     })
 
 }());
