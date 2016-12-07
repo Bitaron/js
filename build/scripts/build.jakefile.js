9b147e945c1c23fdc8079345142f07c34aea2b27
bitaron <bitaron90@gmail.com>
2016-12-07 09:11:14 +0600

    Add node version checker and semver

--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -2,8 +2,16 @@
     'use strict';
 
     desc('This is the default task.');
-    task('default', function () {
+    var startTime = Date.now();
+    task('default', ['version'], function () {
         console.log('This is the default task.');
+        var elapsedSeconds = (Date.now() - startTime)/1000;
+        console.log("\n\nBuild ok ("+ elapsedSeconds.toFixed(2) + "s)");
     });
+    
+    desc('This checks node.js version');
+    task('version', function() {
+        console.log('check nodejs version');
+    })
     
 }());
