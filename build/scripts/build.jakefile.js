2a3b814f5468ef0375fd8c9076f53c1da2fe2218
bitaron <bitaron90@gmail.com>
2016-12-07 09:33:54 +0600

    Add lint task place holder and config file

--- build/scripts/build.jakefile.js
+++ build/scripts/build.jakefile.js
@@ -5,15 +5,14 @@
 
     desc('This is the default task.');
     var startTime = Date.now();
-    task('default', ['version'], function () {
-        console.log('This is the default task.');
+    task('default', ['version', 'lint'], function () {
         var elapsedSeconds = (Date.now() - startTime) / 1000;
         console.log("\n\nBuild ok (" + elapsedSeconds.toFixed(2) + "s)");
     });
 
     desc('This checks node.js version');
     task('version', function () {
-        console.log('check nodejs version');
+        console.log('Checking nodejs version ..');
         versionChecker.check({
             name : 'Node',
             strict : true,
@@ -21,5 +20,18 @@
             expected : require("../../package.json").engines.node
         },complete, fail);
     },{async : true});
+    
+    desc('This lint the code base');
+    task('lint', ['lintBuild', 'lintClient']);
+    
+    desc('This lint build section');
+    task('lintBuild', function() {
+        console.log('Linting build ..');
+    });
+    
+    desc('This lint client section');
+    task('lintClient', function() {
+        console.log('Linting client ..');
+    })
 
 }());
